10354c2ae61c1461998aa0d4d8d92fb542a5656a
nicolasalexandercabezasmonroy2012 <114183851+nicolasalexandercabezasmonroy2012@users.noreply.github.com>
2022-09-23 14:30:08 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function showTitle() {
+    var title = document.getElementById("title");
+    title.style.visibility = "visible";
+  }
+  function unshowTitle() {
+    var title = document.getElementById("title");
+    title.style.visibility = "hidden";
+  }
